30d92deabdbd98e937522da6b14f83c0ae044e6d
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 18:03:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,15 @@ end
 def hit?(card_total)
   prompt_user
   input = get_user_input
-  
+  until input == "h" || input == "s"
+    invalid_command
+    prompt_user
+    input = get_user_input
+  end
+  if input == "h"
+    card_total += deal_card
+  elsif input == "s"
+    card_total
   end
 end
 
